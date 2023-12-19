68f79c688757245fcf5807d9dc0996bda8d09980
yannik <ysinthern@gmail.com>
2023-12-19 09:09:38 +0100

    neuer branch m5

--- emensa-werbeseite/config/db.php
+++ emensa-werbeseite/config/db.php
@@ -2,8 +2,8 @@
 return [
     'host' => 'localhost',  // 'localhost' or '127.0.0.1'
     'user' => 'root',       // '<yourusername>'
-    'password' => '1234', // '<yourpassword>'
+    'password' => 'root', // '<yourpassword>'
     'database' => 'emensawerbeseite',
     // optionally: set port below if it differs from the default 3306
-    'port' => 3307 // !! this is not your webserver port, but the mariadb port
+    'port' => 3306 // !! this is not your webserver port, but the mariadb port
 ];
